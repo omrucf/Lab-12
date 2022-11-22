51db2aa838b39968b31d435ae9699549870d5f87
omrucf <omrucf@aucegypt.edu>
2022-11-22 16:42:22 +0200

    formatted

--- main.cpp
+++ main.cpp
@@ -9,18 +9,18 @@ typedef vector<vector<int>> MTRX;
 
 MTRX getOptimalSplit(int[], int);
 
-void printOptimal(int[], int , int , int);
+void printOptimal(int[], int, int, int);
 
 int main()
 {
-  int b[] = {5,4,6,2,7};
+  int b[] = {5, 4, 6, 2, 7};
   int n = 5;
 
-  printOptimal(b, 1, n-1, n);
-  cout << endl ;
+  printOptimal(b, 1, n - 1, n);
+  cout << endl;
 
-  int a[] = {4,4,6,12,7};
-  printOptimal(a, 1, n-1 ,n);
+  int a[] = {4, 4, 6, 12, 7};
+  printOptimal(a, 1, n - 1, n);
 
   cout << endl;
   return 0;
@@ -29,7 +29,7 @@ int main()
 MTRX getOptimalSplit(int Ns[], int n)
 {
   int M[n][n];
-  MTRX S(n, vector<int> (n));
+  MTRX S(n, vector<int>(n));
   for (int i = 1; i < n; i++)
     M[i][i] = 0;
 
@@ -46,30 +46,30 @@ MTRX getOptimalSplit(int Ns[], int n)
         int q = M[i][k] + M[k + 1][j] + Ns[i - 1] * Ns[k] * Ns[j];
 
         if (q < M[i][j])
-          {
-            M[i][j] = q;
-            S[i][j] = k;
-          }
+        {
+          M[i][j] = q;
+          S[i][j] = k;
+        }
       }
     }
   }
   return S;
 }
 
-void printOptimal(int Ns[], int i , int j, int n)
+void printOptimal(int Ns[], int i, int j, int n)
 {
   MTRX S = getOptimalSplit(Ns, n);
 
   if (i == j)
-    {
-      cout << "A" << i;
-      return;
-    }
-  
-  cout << "(" ;
-  
-  printOptimal(Ns, i , S[i][j], n);
-  printOptimal(Ns, S[i][j]+1, j, n);
+  {
+    cout << "A" << i;
+    return;
+  }
 
-  cout << ")" ;
+  cout << "(";
+
+  printOptimal(Ns, i, S[i][j], n);
+  printOptimal(Ns, S[i][j] + 1, j, n);
+
+  cout << ")";
 }
